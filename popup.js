14726139f19290f56a9defc8eb6e5a94e5f41960
digiborimusik <digiborimusik@gmail.com>
2018-12-10 14:51:09 +0200

    form logic added

--- popup.js
+++ popup.js
@@ -22,15 +22,122 @@ document.querySelector('#buttonTwo').onclick = function(is) {
 };
 document.querySelector('#buttontree').onclick = function(is) {
 	console.log('tree')
-	document.querySelector('.params').style.left = "10vw";
+	// document.querySelector('.params').style.left = "10vw";
+	setTimeout(doThat,200)
+	function doThat(){
+		document.querySelector('.params').style.left = "10vw";
+	}
 };
 
-document.querySelector('#params-hide').onclick = function(is) {
-	console.log('tree')
-	document.querySelector('.params').style.left = "100vw";
+
+
+document.addEventListener("click", function(){
+    // console.log(event.path);
+        // console.log(dd);
+    var dd = event.path[event.path.length - 7];
+    if (dd !== undefined) {
+    	dd = event.path[event.path.length - 7].className
+    }
+    var dds = document.querySelector('.params').style.left;
+    if (dd !== "params" && dds === "10vw" || dd === undefined ) {
+    	document.querySelector('.params').style.left = "100vw";
+    }
+    var a = event.path[0].id.slice(0,3);
+    if (a === "cbx") {
+    	checkbox(event.path[0].id.slice(3,4))
+    }
+    if (a === "res") {
+    	resetf(event.path[0].id.slice(8,9))
+    }
+
+});
+
+
+function checkbox(n){
+	var cbx = document.getElementById("cbx" + [n]);
+	if (cbx.checked == true){
+		document.querySelectorAll('form')[n-1].querySelectorAll('.input-text')[0].disabled = false
+		document.querySelectorAll('form')[n-1].querySelectorAll('.input-text')[1].disabled = false
+        document.querySelectorAll('form')[n-1].querySelectorAll('select')[0].disabled = false
+        document.querySelectorAll('form')[n-1].querySelectorAll('select')[1].disabled = false
+    } else {
+      	document.querySelectorAll('form')[n-1].querySelectorAll('.input-text')[0].disabled = true
+		document.querySelectorAll('form')[n-1].querySelectorAll('.input-text')[1].disabled = true
+        document.querySelectorAll('form')[n-1].querySelectorAll('select')[0].disabled = true
+        document.querySelectorAll('form')[n-1].querySelectorAll('select')[1].disabled = true
+    }
+}
+
+function resetf(n){
+		console.log(n);
+      	document.querySelectorAll('form')[n-1].querySelectorAll('.input-text')[0].disabled = true
+		document.querySelectorAll('form')[n-1].querySelectorAll('.input-text')[1].disabled = true
+        document.querySelectorAll('form')[n-1].querySelectorAll('select')[0].disabled = true
+        document.querySelectorAll('form')[n-1].querySelectorAll('select')[1].disabled = true
+}
+
+function collectData(){
+	for (var i = 4; i >= 0; i--) {
+		var check = document.getElementById("cbx" + [i+1]).checked;
+		storage.user[i].checkd = check;
+		var login = document.querySelectorAll('form')[i].querySelectorAll('.input-text')[0].value;
+		storage.user[i].login = login;
+		var pass = document.querySelectorAll('form')[i].querySelectorAll('.input-text')[1].value;
+		storage.user[i].pass = pass;
+		var cat1 = document.querySelectorAll('form')[i].querySelectorAll('select')[0].value;
+		storage.user[i].cat1 = cat1;
+		var cat2 = document.querySelectorAll('form')[i].querySelectorAll('select')[0].value;
+		storage.user[i].cat2 = cat2;
+	}
+}
+
+function reloadData(){
+	for (var i = 4; i >= 0; i--) {
+		document.getElementById("cbx" + [i+1]).checked = storage.user[i].checkd;
+		document.querySelectorAll('form')[i].querySelectorAll('.input-text')[0].value = storage.user[i].login;
+		document.querySelectorAll('form')[i].querySelectorAll('.input-text')[1].value = storage.user[i].pass;
+		document.querySelectorAll('form')[i].querySelectorAll('select')[0].value = storage.user[i].cat1;
+		document.querySelectorAll('form')[i].querySelectorAll('select')[0].value = storage.user[i].cat2;
+		if (storage.user[i].checkd) {
+			console.log(i)
+			document.querySelectorAll('form')[i].querySelectorAll('.input-text')[0].disabled = false
+			document.querySelectorAll('form')[i].querySelectorAll('.input-text')[1].disabled = false
+        	document.querySelectorAll('form')[i].querySelectorAll('select')[0].disabled = false
+        	document.querySelectorAll('form')[i].querySelectorAll('select')[1].disabled = false
+		}
+	}
+}
+
+storage = {
+	user:{
+		0:{},
+		1:{},
+		2:{},
+		3:{},
+		4:{}
+	}
 };
 
+function storageSet(key,prop){
+	chrome.storage.local.set({[key]:prop});
+}
 
+
+function storageUpdate(key){
+	chrome.storage.local.get([key], function(result) {
+        storage = result[key];
+        console.log(result)
+        });
+}
+
+function saveData(){
+	collectData();
+	storageSet("user",storage);
+}
+function loadData(){
+	storageUpdate("user");
+	setTimeout(reloadData,1000)
+}
 
 function addLi(text){
 	var node = document.createElement("LI");
