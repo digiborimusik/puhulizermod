a2e29d6ca93d2798e62ffc6c79b4136a2cf23a59
digiborimusik <digiborimusik@gmail.com>
2019-04-02 16:41:04 +0300

    alphabet func

--- background.js
+++ background.js
@@ -20,6 +20,20 @@ var theKatas = (function(){
 		
 	}
 
+
+	function calcAlphabet (str) {
+		var total = 0;
+		var strArr = str.split("")
+		var alphabet = { a:1 , b:2 , c:3 , d:4 , e:5, f:6 , g:7, h:8, i:9 , j:10 , k:11 , l:12, m:13, n:14 , o:15, p:16 ,q:17 , r:18 , s:19, t:20, u:21, v:22 , w:23 , x:24, y:25 , z:26}
+	  
+		for (var i = strArr[0]; i<strArr.length ; i++){
+			total = total + alphabet[i]
+		}
+		return total;
+	  
+		calcAlphabet("example")
+	}
+
 	return {
 		help:function(){
 			return information
